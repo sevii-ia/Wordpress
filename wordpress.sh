7a7074354710ffad82cbe1e18be41a50790a3a65
Vsevolod <147947186+sevii-ia@users.noreply.github.com>
2025-02-01 10:38:24 +0100

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,12 +1,12 @@
 sudo apt update -y
-sudo apt install -y nginx
-sudo systemctl start nginx && systemctl enable nginx
+sudo apt install -y apache2
+sudo systemctl start apache2 && systemctl enable apache2
 sudo apt install -y php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo apt install -y mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
 
 sudo mysql -u root <<MYSQL_SCRIPT
-CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'YourStrongPassword';
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'Vvss0823';
 CREATE DATABASE wordpress;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost';
 FLUSH PRIVILEGES;
@@ -20,4 +20,4 @@ mv /home/ubuntu/Desktop/wp-config.php /var/www/html/wordpress/wp-config.php
 
 mv /home/ubuntu/Desktop/wordpress.conf /etc/nginx/conf.d/wordpress.conf
 
-sudo systemctl restart nginx
+sudo systemctl restart apache2
