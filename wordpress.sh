062652c5ce037ef4f4abcad7761d9cd4ca3d0eb4
Vsevolod <147947186+sevii-ia@users.noreply.github.com>
2025-02-01 11:01:31 +0100

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,6 +1,6 @@
 sudo apt update -y
-sudo apt install -y apache2
-sudo systemctl start apache2 && systemctl enable apache2
+sudo apt install -y nginx
+sudo systemctl start nginx && systemctl enable nginx
 sudo apt install -y php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo apt install -y mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
@@ -20,4 +20,4 @@ mv /home/ubuntu/Wordpress/wp-config.php /var/www/html/wordpress/wp-config.php
 
 mv /home/ubuntu/Wordpress/wordpress.conf /etc/nginx/conf.d/wordpress.conf
 
-sudo systemctl restart apache2
+sudo systemctl restart nginx
